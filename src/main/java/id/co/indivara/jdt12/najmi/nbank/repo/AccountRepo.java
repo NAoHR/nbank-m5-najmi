5d72f84b751f954d28e5fba6f4a929c5661ae8c5
naohr <najmim625@gmail.com>
2023-06-14 23:42:40 +0700

    add findAllByCustomer method to AccountRepo Interface

--- src/main/java/id/co/indivara/jdt12/najmi/nbank/repo/AccountRepo.java
+++ src/main/java/id/co/indivara/jdt12/najmi/nbank/repo/AccountRepo.java
@@ -1,10 +1,13 @@
 package id.co.indivara.jdt12.najmi.nbank.repo;
 
 import id.co.indivara.jdt12.najmi.nbank.entity.Account;
+import id.co.indivara.jdt12.najmi.nbank.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 
 public interface AccountRepo extends JpaRepository<Account, UUID> {
+    List<Account> findAllByCustomer(Customer c);
 }
