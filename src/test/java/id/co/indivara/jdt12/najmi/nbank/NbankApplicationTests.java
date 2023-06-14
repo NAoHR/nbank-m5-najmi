40c8c96f330f9a9697b03ddb070fb4a3bc1f3e5a
naohr <najmim625@gmail.com>
2023-06-14 11:35:08 +0700

    update: config and test file

---
+++ src/test/java/id/co/indivara/jdt12/najmi/nbank/NbankApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.indivara.jdt12.najmi.nbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
