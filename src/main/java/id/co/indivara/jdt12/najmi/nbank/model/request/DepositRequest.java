816a8a6fd9156f1a93b0398493d3fc3aa8a133ac
naohr <najmim625@gmail.com>
2023-06-19 14:33:37 +0700

    set minimal money to be 0 for no negative number validation

--- src/main/java/id/co/indivara/jdt12/najmi/nbank/model/request/DepositRequest.java
+++ src/main/java/id/co/indivara/jdt12/najmi/nbank/model/request/DepositRequest.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -17,6 +18,7 @@ public class DepositRequest {
     @NotNull
     private UUID uid;
     @NotNull
+    @Min(0)
     private BigDecimal money;
 
 }
