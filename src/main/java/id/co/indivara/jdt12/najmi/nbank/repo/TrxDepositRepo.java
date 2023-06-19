8f2943ac7233b0f4762b6566c8a2f2a498082906
naohr <najmim625@gmail.com>
2023-06-19 14:30:11 +0700

    inject custom query for 24 hours transaction

--- src/main/java/id/co/indivara/jdt12/najmi/nbank/repo/TrxDepositRepo.java
+++ src/main/java/id/co/indivara/jdt12/najmi/nbank/repo/TrxDepositRepo.java
@@ -3,10 +3,17 @@ package id.co.indivara.jdt12.najmi.nbank.repo;
 import id.co.indivara.jdt12.najmi.nbank.entity.Account;
 import id.co.indivara.jdt12.najmi.nbank.entity.TrxDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.List;
 
 
 public interface TrxDepositRepo extends JpaRepository<TrxDeposit, Long> {
     List<TrxDeposit> findAllByAccount(Account ac);
+
+    @Query(value = "SELECT SUM(t.amount) FROM TrxDeposit t WHERE EXTRACT(epoch FROM (DATE_TRUNC('day', CURRENT_TIMESTAMP) - DATE_TRUNC('day', t.timestamp))) < 1 AND t.account = ?1")
+    BigDecimal getTodayTransaction(Account account);
 }
--- src/main/java/id/co/indivara/jdt12/najmi/nbank/repo/TrxTransferRepo.java
+++ src/main/java/id/co/indivara/jdt12/najmi/nbank/repo/TrxTransferRepo.java
@@ -3,10 +3,15 @@ package id.co.indivara.jdt12.najmi.nbank.repo;
 import id.co.indivara.jdt12.najmi.nbank.entity.Account;
 import id.co.indivara.jdt12.najmi.nbank.entity.TrxTransfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
 public interface TrxTransferRepo extends JpaRepository<TrxTransfer, Long> {
     List<TrxTransfer> findAllByAccount(Account ac);
+
+    @Query(value = "SELECT SUM(t.amount) FROM TrxTransfer t WHERE EXTRACT(epoch FROM (DATE_TRUNC('day', CURRENT_TIMESTAMP) - DATE_TRUNC('day', t.timestamp))) < 1 AND t.account = ?1")
+    BigDecimal getTodayTransaction(Account account);
 }
