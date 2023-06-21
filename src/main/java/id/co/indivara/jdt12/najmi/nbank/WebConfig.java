75145dd171042c6555e37973a045262cabb5dcd9
naohr <najmim625@gmail.com>
2023-06-21 23:56:14 +0700

    add atm endpoint to interceptor

--- src/main/java/id/co/indivara/jdt12/najmi/nbank/WebConfig.java
+++ src/main/java/id/co/indivara/jdt12/najmi/nbank/WebConfig.java
@@ -24,6 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(accountSecurityInterceptor)
                 .addPathPatterns("/api/auth/account/logout")
+                .addPathPatterns("/api/atm/**")
         ;
     }
 }
