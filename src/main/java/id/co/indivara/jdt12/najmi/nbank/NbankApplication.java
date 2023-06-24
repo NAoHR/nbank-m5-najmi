0d5d49ff10dbf01aec90d99dd8cfd7d1283cbd3b
naohr <najmim625@gmail.com>
2023-06-25 05:10:19 +0700

    setup war package

--- src/main/java/id/co/indivara/jdt12/najmi/nbank/NbankApplication.java
+++ src/main/java/id/co/indivara/jdt12/najmi/nbank/NbankApplication.java
@@ -3,13 +3,19 @@ package id.co.indivara.jdt12.najmi.nbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class NbankApplication {
+public class NbankApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(NbankApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NbankApplication.class);
+	}
 }
